482e7327596889be9cf4f472090fa7fa67fca40c
mayabridgewater <55682563+mayabridgewater@users.noreply.github.com>
2020-01-23 15:08:03 +0200

    add winning number to current players game

--- server-demo/db/win.js
+++ server-demo/db/win.js
@@ -2,7 +2,7 @@ const {connection} = require('./config');
 
 function addWin(num, id) {
     return new Promise((resolve, reject) => {
-        connection.query('update game_history set player_number = ? where id = ?', [num, id], function(error, results, fields) {
+        connection.query('update game_history set player_number = ? where player_id = ? order by `date` desc limit 1', [num, id], function(error, results, fields) {
             if(error) {
                 reject(error)
             }else {
