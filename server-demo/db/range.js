aa7225a7292d3375514a2d181551a478b75b6232
mayabridgewater <55682563+mayabridgewater@users.noreply.github.com>
2020-01-23 14:53:40 +0200

    player response not on server

--- server-demo/db/range.js
+++ server-demo/db/range.js
@@ -2,7 +2,7 @@ const {connection} = require('./config');
 
 function getRange(id) {
     return new Promise((resolve, reject) => {
-        connection.query('select `range` from game_history where player_id = ?', [id], function(error, results, fields) {
+        connection.query('select `range` from game_history where player_id = ? order by `date` desc limit 1', [id], function(error, results, fields) {
             if (error) {
                 reject(error)
             }else {
